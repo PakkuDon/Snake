435717f2d644cd47b1799c3de70587a02ab8b72f
PakkuDon <pakku.pd@gmail.com>
2015-04-22 23:47:08 +1000

    Added GameModel class to track and manipulate game state

---
+++ src/model/GameModel.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.util.List;
+import java.util.Random;
+
+public class GameModel {
+    // Constants
+    private static final int INITIAL_SNAKE_LENGTH = 3;
+   
+    // Instance variables
+    private Snake snake;
+    private Food food;
+    private Random rand;
+    private int width;
+    private int height;
+    private int score; 
+   
+    // Constructor
+    public GameModel(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.rand = new Random();
+        
+        this.start();
+    }
+    
+    // Getters
+    public List<Snake.Node> getSnake() { 
+        return snake.getNodes();
+    }
+    
+    public Food getFood() {
+        return food;
+    }
+    
+    public int getScore() {
+        return score;
+    }
+    
+    // Actions
+    public void start() {
+        snake = new Snake(this.width / 2, this.height / 2, INITIAL_SNAKE_LENGTH);
+    }
+    
+    public void spawnFood() {
+        food.setX(rand.nextInt(width));
+        food.setY(rand.nextInt(height));
+    }
+    
+    public void move() {
+        snake.move();
+    }
+    
+    public void setSnakeDirection(Direction direction) {
+        snake.setDirection(direction);
+    }
+}

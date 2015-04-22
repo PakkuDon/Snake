fd9a83adb11a11d7a500d55839955f22b03ab302
PakkuDon <pakku.pd@gmail.com>
2015-04-22 22:36:03 +1000

    Initial commit

---
+++ src/main/AppInit.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Application entry-point
+ * Responsible for initialising model and view.
+ */
+public class AppInit {
+    public static void main(String[] args) {
+        
+    }
+}
